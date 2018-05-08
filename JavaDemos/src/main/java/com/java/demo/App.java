adb571f8ce8dec15b424e9219603f4911e385c53
fangshanshangithub <643915247@qq.com>
2018-05-08 21:31:17 +0800

    2018-54-08 公司中 commit

--- JavaDemos/src/main/java/com/java/demo/App.java
+++ JavaDemos/src/main/java/com/java/demo/App.java
@@ -6,6 +6,10 @@ package com.java.demo;
  */
 public class App 
 {
+    /**
+     *
+     * @param args
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
