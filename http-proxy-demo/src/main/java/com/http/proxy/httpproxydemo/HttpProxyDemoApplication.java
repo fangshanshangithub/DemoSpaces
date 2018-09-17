5ae9201e806d4161fb8b5bad0a290b9e0822715c
FangShanShanFamily <643915247@qq.com>
2018-09-17 22:50:23 +0800

    20180917 home commit

---
+++ http-proxy-demo/src/main/java/com/http/proxy/httpproxydemo/HttpProxyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.http.proxy.httpproxydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpProxyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpProxyDemoApplication.class, args);
+    }
+}
