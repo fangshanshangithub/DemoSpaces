80aa226ad4d7e0ef923ee7758bf6ca1fc402737c
FangShanShanFamily <643915247@qq.com>
2018-09-27 22:21:43 +0800

    20180927 home commit

--- http-proxy-demo/src/main/java/com/http/proxy/httpproxydemo/controller/HttpProxyController.java
+++ http-proxy-demo/src/main/java/com/http/proxy/httpproxydemo/controller/HttpProxyController.java
@@ -1,6 +1,10 @@
 package com.http.proxy.httpproxydemo.controller;
 
+import com.http.proxy.httpproxydemo.utils.HttpClientUtils;
 import com.http.proxy.httpproxydemo.utils.HttpsUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HttpProxyController {
@@ -23,7 +30,34 @@ public class HttpProxyController {
         out.write(result);
         out.flush();
         out.close();
- 
+    }
 
+    @RequestMapping("get_test")
+    public void getTest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, String> headerMap =  getHeadersInfo(request);
+        response.reset();
+        HttpResponse resultResponse = HttpClientUtils.doGet("https://www.qidian.com/","",headerMap, null);
+        String srtResult = EntityUtils.toString(resultResponse.getEntity());//获得返回的结果
+        Header[] headers = resultResponse.getAllHeaders();
+        for(int i=0;i<headers.length;i++) {
+            System.out.println(headers[i].getName()+" : "+ headers[i].getValue());
+            response.setHeader(headers[i].getName(), headers[i].getValue());
+        }
+
+        PrintWriter out = response.getWriter();
+        out.write(srtResult);
+        out.flush();
+        out.close();
+    }
+
+    private Map<String, String> getHeadersInfo(HttpServletRequest request) {
+        Map<String, String> map = new HashMap<String, String>();
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        return map;
     }
 }
